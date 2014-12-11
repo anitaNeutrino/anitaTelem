aaf66be3226b033e36342943084b28923a8514e0
radio <radio@anitarf.ldb>
2014-12-12 12:13:08 +1300

    Updating the AnitaFileHandler.h, there was a bug in the previous incarnation

--- copyCmdSendFromTdrss.sh
+++ copyCmdSendFromTdrss.sh
@@ -1,13 +1,21 @@
 #!/bin/bash
 source /home/radio/anita14/anitaTelem/setupAwareVariablesForTelem.sh
-TDRSS_HOST=192.168.1.6
+TDRSS_HOST=tdrss
 TDRSS_USER=anita
 TDRSS_DIR=/home/anita/cmdSend/jsonLog/
-DEST_DIR=$AWARE_OUTPUT_DIR/ANITA3/cmdSend/tdrss/
-SRC_DIR=${TDRSS_USER}@${TDRSS_HOST}:${TDRSS_DIR}/*.json
+TDRSS_DEST_DIR=$AWARE_OUTPUT_DIR/ANITA3/cmdSend/palestine
+TDRSS_SRC_DIR=${TDRSS_USER}@${TDRSS_HOST}:${TDRSS_DIR}/*.json
+
+LOS_HOST=192.168.1.6
+LOS_USER=anita
+LOS_DIR=/home/anita/cmdSend/jsonLog/
+LOS_DEST_DIR=$AWARE_OUTPUT_DIR/ANITA3/cmdSend/los
+LOS_SRC_DIR=${LOS_USER}@${LOS_HOST}:${LOS_DIR}/*.json
 
 
 while [ 1 ]; do
-    rsync -av $SRC_DIR $DEST_DIR
+    date
+    rsync -av $TDRSS_SRC_DIR $TDRSS_DEST_DIR
+    rsync -av $LOS_SRC_DIR $LOS_DEST_DIR
     sleep 30
 done
