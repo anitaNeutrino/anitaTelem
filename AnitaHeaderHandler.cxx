71e27f1f93abe3da5829e15f231a7953db616beb
Ryan Nichol <r.nichol@ucl.ac.uk>
2014-12-20 10:42:14 +0000

    Switching to default calib

--- AnitaHeaderHandler.cxx
+++ AnitaHeaderHandler.cxx
@@ -608,7 +608,7 @@ void AnitaHeaderHandler::plotEvent(AnitaEventHeader_t *hdPtr,PedSubbedEventBody_
     fMagicCanvas->Update();
   } 
   
-  UsefulAnitaEvent *usefulEventPtr = new UsefulAnitaEvent(fTheEvent,WaveCalType::kVoltageTime);  
+  UsefulAnitaEvent *usefulEventPtr = new UsefulAnitaEvent(fTheEvent,WaveCalType::kDefault);  
   
 
 
