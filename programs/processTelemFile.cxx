3094c67a1f4e653ca0bd435a5abbbf4badae05ea
Ryan Nichol <r.nichol@ucl.ac.uk>
2016-12-02 11:57:29 +0000

    Fixing iridium crash

--- programs/processTelemFile.cxx
+++ programs/processTelemFile.cxx
@@ -141,8 +141,8 @@ int main (int argc, char ** argv)
   double val=gRandom->Rndm();
   int plotEvents=0;
 
-  //  if(val>0.3) plotEvents=1;
-    plotEvents=1;
+  if(val>0.3) plotEvents=1;
+  //  plotEvents=1;
   if(plotEvents) std::cout << "Plotting events\n";
   else std::cout << "Not plotting events\n";
 
