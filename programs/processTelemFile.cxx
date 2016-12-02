cf11160189a16ceaca4dd21c30d9609c2e4fe974
Ryan J. Nichol <r.nichol@ucl.ac.uk>
2016-12-02 02:07:33 +0000

    Turing on event plotting

--- programs/processTelemFile.cxx
+++ programs/processTelemFile.cxx
@@ -142,7 +142,7 @@ int main (int argc, char ** argv)
   int plotEvents=0;
 
   //  if(val>0.3) plotEvents=1;
-    plotEvents=1;
+  plotEvents=1;
   if(plotEvents) std::cout << "Plotting events\n";
   else std::cout << "Not plotting events\n";
 
